e72cef7cd427ac4e598c074faf4e493fa707d3bc
pgw6541 <pgw6541@gmail.com>
2022-05-15 03:21:29 +0900

    About, Home으로 수정 및 Roter 모드 수정

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -38,7 +38,7 @@ export default {
     return {
       navigations: [
         {
-          name: 'About',
+          name: 'Home',
           href: '/'
         },
         {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Products from './Products'
-import About from './About'
+import Home from './Home'
 import Interests from './Interests'
 import Interest_game from './Interest_game'
 import Interest_movie from './Interest_movie'
@@ -9,13 +9,13 @@ export default createRouter({
   // Hash모드, History모드로 사용 가능
   // https://google.com/#/search에서 #기법을 이용
   // 기본적으로 브라우저(IE든 크롬이든 Edge든 뭐든)는 # 이 붙은 URL의 뒷내용은 인지하지 못함. 즉, 일반적인 경우에 # 뒤의 값이 변하더라도 브라우저는 새로운 화면을 로드하지 않음.
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   // page들을 구분해주는 옵션
   routes: [
     {
       // '/' = path는 해당 URL 뒤쫏에 붙음 따라서 /공백 은 메인페지이로 이동 ex) https://google.com/
       path: '/',
-      component: About,
+      component: Home,
     },
     {
       path: '/products',
