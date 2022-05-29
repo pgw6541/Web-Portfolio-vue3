2b0b85b2586691f9e298f7a626daccb7c079969f
pgw6541 <pgw6541@gmail.com>
2022-05-29 17:46:05 +0900

    Works Hover

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -4,15 +4,22 @@
     <a
       class="workBox"
       v-for="work in works"
-      :key="work"
-      :href="work.href"
-      target="_blank">
-      <img
-        :src="work.image"
-        alt="Work" />
+      :key="work">
+      <div class="img">
+        <img
+          :src="work.image"
+          alt="Work" />
+        <div class="lines"></div>
+        <div class="angles"></div>
+        <div class="info">
+          <p>{{ work.info }}</p>
+          <a
+            :href="work.href"
+            target="_blank">Go to Site</a>
+        </div>
+      </div>
       <div class="textBox">
         <h1>{{ work.name }}</h1>
-        <p>{{ work.info }}</p>
       </div>
     </a>
     <!-- Skeleton -->
@@ -35,11 +42,13 @@ export default {
           name: 'OMDbApi 영화 검색 사이트',
           href: 'https://movie-search-app-vue3.netlify.app',
           image: 'https://raw.githubusercontent.com/pgw6541/image_source/master/Web-Portfolio/omdb.png',
+          info: '검색창에 영화 검색하면 영화나옴'
         },
         {
           name: '2017 Starbucks Home',
           href: 'https://starbucks-gunw.netlify.app',
           image: 'https://raw.githubusercontent.com/pgw6541/image_source/master/Web-Portfolio/starbucks.png',
+          info: '2017년도 스타벅스 메인페이지 클론코딩'
         },
       ]
     }
@@ -48,6 +57,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$ease-in-out-quad: cubic-bezier(0.455, 0.03, 0.515, 0.955);
+$color-grey: #A6AFC0;
+$color-off-white: $gray-700;
 //Works img
 $imgSize: 300px;
 $xxl-imgSize: 250px;
@@ -61,12 +73,78 @@ $sm-imgSize: 180px;
   .workBox {
     text-decoration: none;
     margin: 50px 20px 0 20px;
-    img {
-      width: $imgSize * 16/9;
-      height: $imgSize;
+    .img {
+      position: relative;
+      img {
+        width: $imgSize * 16/9;
+        height: $imgSize;
+        // border-radius: 10px;
+        }
+      .lines {
+        background:
+          linear-gradient(to bottom, $color-off-white 2px, transparent 2px) 0 0, // TOP LEFT TO TOP RIGHT
+          linear-gradient(to right, $color-off-white 2px, transparent 2px) 0 100%, // BOTTOM LEFT TO TOP LEFT
+          linear-gradient(to left, $color-off-white 2px, transparent 2px) 100% 0, // TOP RIGHT TO BOTTOM RIGHT
+          linear-gradient(to top, $color-off-white 2px, transparent 2px) 100% 100%; // BOTTOM RIGHT TO TOP RIGHT
+        background-repeat: no-repeat;
+        background-size: 0px 0px;
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        transition: 0.2s $ease-in-out-quad;
+        margin: -10px;
+      }
+      .angles {
+        background:
+          linear-gradient(to right, $color-grey 2px, transparent 2px) 0 0, // TOP LEFT
+          linear-gradient(to bottom, $color-grey 2px, transparent 2px) 0 0, // TOP LEFT
+          linear-gradient(to left, $color-grey 2px, transparent 2px) 100% 0, // TOP RIGHT
+          linear-gradient(to bottom, $color-grey 2px, transparent 2px) 100% 0, // TOP RIGHT
+          linear-gradient(to left, $color-grey 2px, transparent 2px) 100% 100%, // BOTTOM RIGHT
+          linear-gradient(to top, $color-grey 2px, transparent 2px) 100% 100%, // BOTTOM RIGHT
+          linear-gradient(to right, $color-grey 2px, transparent 2px) 0 100%, // BOTTOM LEFT
+          linear-gradient(to top, $color-grey 2px, transparent 2px) 0 100%; // BOTTOM LEFT
+        background-repeat: no-repeat;
+        background-size: 7px 7px;
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        opacity: 0.48;
+        transition: 0.2s $ease-in-out-quad;
+        margin: -10px;
+      }
+      .info {
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0,0,0);
+        position: absolute;
+        top: 0;
+        left: 0;
+        transition: .3s;
+        opacity: 0;
+        p {
 
-      border: 2px solid $gray-200;
-      border-radius: 10px;
+        }
+        a {
+
+        }
+      }
+      &:hover {
+        .lines {
+          background-size: 100% 100%;
+        }
+        .angles {
+          background-size: 14px 14px;
+        }
+        .info {
+          background-color: rgba(0,0,0);
+          opacity: .8;
+        }
+      }
     }
     .textBox {
       text-align: center;
@@ -74,7 +152,7 @@ $sm-imgSize: 180px;
         color: $gray-500;
         font-size: 15px;
         font-weight: 700;
-        margin-top: 15px;
+        margin-top: 20px;
         box-sizing: border-box;
       }
     }
@@ -94,9 +172,11 @@ $sm-imgSize: 180px;
   }
   @include media-breakpoint-down(xxl) {
     .workBox {
-      img {
-        width: $xxl-imgSize * 16/9;
-        height: $xxl-imgSize;
+      .img {
+        img {
+          width: $xxl-imgSize * 16/9;
+          height: $xxl-imgSize;
+        }
       }
     }
     .skeletons {
@@ -108,9 +188,11 @@ $sm-imgSize: 180px;
   }
   @include media-breakpoint-down(xl) {
     .workBox {
-      img {
-        width: $xl-imgSize * 16/9;
-        height: $xl-imgSize;
+      .img {
+        img {
+          width: $xl-imgSize * 16/9;
+          height: $xl-imgSize;
+        }
       }
     }
     .skeletons {
@@ -122,9 +204,11 @@ $sm-imgSize: 180px;
   }
   @include media-breakpoint-down(lg) {
     .workBox {
-      img {
-        width: $imgSize * 16/9;
-        height: $imgSize;
+      .img {
+        img {
+          width: $imgSize * 16/9;
+          height: $imgSize;
+        }
       }
     }
     .skeletons {
@@ -136,9 +220,11 @@ $sm-imgSize: 180px;
   }
   @include media-breakpoint-down(sm) {
     .workBox {
-      img {
-        width: $sm-imgSize * 16/9;
-        height: $sm-imgSize;
+      .img {
+        img {
+          width: $sm-imgSize * 16/9;
+          height: $sm-imgSize;
+        }
       }
     }
     .skeletons {
